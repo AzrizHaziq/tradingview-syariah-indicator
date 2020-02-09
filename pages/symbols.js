8db15390d54094e69236262d522b017118a675d6
AzrizHaziq <azrizhaziq@gmail.com>
2020-02-09 13:30:20 +0800

    Going to remove old way to add icon since chrome have error load with img.src="xxxx.svg"
    - using string svg icon and inject to dom

--- pages/symbols.js
+++ pages/symbols.js
@@ -5,31 +5,33 @@ if (browser.runtime.onMessage.hasListener(receiveSignalFromBgScript)) {
 
 browser.runtime.onMessage.addListener(receiveSignalFromBgScript)
 
+addStaticSyariahIcon();
+
 function receiveSignalFromBgScript({ list: SYARIAH_COMPLIANCE_LIST }) {
   const found = SYARIAH_COMPLIANCE_LIST.find(i => i.id === `${ TRADING_VIEW_MYR }:${ getSymbols() }`)
 
   if (found.syariah) {
-    const largeResoDom = document.querySelector('h1 div.tv-symbol-header__short-title')
-    const smallResoDom = document.querySelector('.tv-symbol-header__text-group--mobile .tv-symbol-header__short-title')
+    const largeResoDom = document.querySelector('.tv-symbol-header__short-title.tv-symbol-header__short-title--with-icon')
+    const smallResoDom = document.querySelector('.tv-symbol-header__text-group--mobile .tv-symbol-header__short-title.tv-symbol-header__short-title--with-icon')
 
     if (isSyariahIconExist(smallResoDom)) {
       // if icon already exist dont do anything
     } else {
-      smallResoDom.insertAdjacentElement('beforeend', syariahIcon({
-        top: '-15px',
-        marginLeft: '3px',
-        position: 'relative',
-      }))
+      const icon = createIcon();
+      icon.style.marginLeft = '5px'
+      icon.style.display = 'inline'
+      icon.style.position = 'relative'
+      icon.style.bottom = '10px'
+
+      smallResoDom.insertAdjacentElement('beforeend', icon)
     }
 
-    if (isSyariahIconExist(largeResoDom)) {
+    if (isSyariahIconExist(largeResoDom.parentElement)) {
       // if icon already exist dont do anything
     } else {
-      largeResoDom.insertAdjacentElement('beforeend', syariahIcon({
-        top: '2px',
-        marginLeft: '3px',
-        position: 'relative',
-      }))
+      const icon = createIcon({ width: 15, height: 15 });
+      icon.style.marginLeft = '5px';
+      largeResoDom.insertAdjacentElement('afterend', icon)
     }
 
   } else {
