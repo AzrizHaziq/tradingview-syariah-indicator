07d6d0d8e5578284fb3a798e240a33a086a1c6dc
azrizhaziq <azrizhaziq@gmail.com>
2021-03-24 15:49:09 +0800

    Done high fidelity design

--- packages/extension/src/popup/index.tsx
+++ packages/extension/src/popup/index.tsx
@@ -2,8 +2,6 @@ import React from 'react'
 import { render } from 'react-dom'
 
 import '../_global.scss'
+import './_index.scss'
 import Popup from './Popup/Popup'
-
 render(<Popup />, window.document.querySelector('#root'))
-
-// if (module.hot) module.hot.accept()
