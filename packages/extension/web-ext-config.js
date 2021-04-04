91f942b0af8f68212d88ff7c4e18964f51865274
azrizhaziq <azrizhaziq@gmail.com>
2021-04-04 17:07:59 +0800

    Update npm scripts
    - update web-ext-config.js

---
+++ packages/extension/web-ext-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  sourceDir: 'dist',
+  run: {
+    startUrl: [
+      'https://www.tradingview.com/symbols/MYX-K1/',
+      'about:debugging#/runtime/this-firefox',
+      // 'chrome://extension',
+    ],
+  },
+}
