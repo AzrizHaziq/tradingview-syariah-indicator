bb766f862afb554971163099e8ef9cdc63d81712
azrizhaziq <azrizhaziq@gmail.com>
2021-11-06 14:26:14 +0800

    delete web/dist

--- packages/web/dist/server/assets/_default.page.client.6894c7bd.js
+++
@@ -1,3 +0,0 @@
-'use strict';
-
-throw new Error('[vite-plugin-ssr][Wrong Usage] File /Users/haziq.jasni/work/tradingview-syariah-indicator/packages/web/renderer/_default.page.client.tsx should not be loaded in Node.js');
