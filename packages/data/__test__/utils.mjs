f7156be8a4b7f0cc16818cab42ead6f67d1a0415
azrizhaziq <azrizhaziq@gmail.com>
2022-03-31 09:52:06 +0800

    update prettier all

--- packages/data/__test__/utils.mjs
+++ packages/data/__test__/utils.mjs
@@ -1,16 +1,16 @@
 import test from 'ava'
 import { pipe, delay } from '../scripts/utils.mjs'
 
-test('pipe', t => {
+test('pipe', (t) => {
   t.plan(1)
-  const result = pipe(data => `Hello ${data}`)('Worlds')
+  const result = pipe((data) => `Hello ${data}`)('Worlds')
   t.is(result, 'Hello Worlds')
 })
 
-test('delay', t => {
+test('delay', (t) => {
   t.plan(1)
 
-  return delay(1).then(n => {
+  return delay(1).then((n) => {
     t.is(1, 1)
   })
 })
