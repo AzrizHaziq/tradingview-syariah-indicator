e22fad5f88f22144d5f7ea74d28f7382f802cd45
azrizhaziq <azrizhaziq@gmail.com>
2022-04-06 15:18:03 +0800

    Update script flow and should be ok now
    - update env variables

--- packages/extension/firefox-source-code.ts
+++ packages/extension/firefox-source-code.ts
@@ -43,20 +43,32 @@ async function createReadme() {
     const readme = `
 ## Tradingview Shariah Indicator (${pkg.version})
 Requirements
-- node = 16.9.1
+- node = 16.14.0
 - pnpm = 6.24.1
 - git = 2.31.0
 
 Steps
-1. Git clone \`git@github.com:AzrizHaziq/tradingview-syariah-indicator.git\`
-2. cd tradingview-shariah-indicator && pnpm install
-3. cd packages/extension
+1. Using Git clone
+   1. \`$ git clone git@github.com:AzrizHaziq/tradingview-syariah-indicator.git\` 
+   2. \`$ cd tradingview-shariah-indicator\`
+   3. \`$ pnpm install\`
+   4. \`$ cd packages/extension\`
+   5. Please continue to step 3 below:
+
+2. Using source-code zip 
+   1. Unzip the \`firefox-tsi-${pkg.version}-source-code.zip\`
+   2. cd inside \`firefox-tsi-${pkg.version}\`
+   3. \`$ pnpm install\`
+   4. Please continue to step 3 below:
+
 3. create \`.env.production\` file in root, and add this
    \`\`\`
    FETCH_URL=https://raw.githubusercontent.com/AzrizHaziq/tradingview-syariah-indicator/master/packages/data/stock-list.json
    \`\`\`
-4. Type in terminal \`$ npm run ff:build\`
-5. Generated file located in /dist/*
+4. Type in terminal \`$ npm run build:firefox\`
+5. Generated: 
+   1. zip file located in \`/web-ext-artifacts/firefox-tsi-${pkg.version}.zip\`
+   2. pure html located in \`/dist/\`
 `.trim()
 
     fs.writeFileSync(`${sourceFolder}/README.md`, readme, {
@@ -67,6 +79,39 @@ Steps
     console.log(`Success write to README.md`)
   } catch (e) {
     throw new Error(`Error create README.md: ${e}`)
+  }
+}
+
+function updatePackageDevDependencies() {
+  try {
+    const { devDependencies } = pkg
+    // list lib to be removed, this will make faster install process
+    // and it wasn't needed in webpack bundling process
+    const blackList = [
+      'prettier',
+      'playwright-chromium',
+      'express',
+      'eslint',
+      'download',
+      'chrome-webstore-upload-cli',
+      'body-parser',
+      'safe-compare',
+      '@playwright/test',
+      '@typescript-eslint',
+    ]
+
+    const removeBlackList = Object.fromEntries(
+      Object.entries(devDependencies).filter(([lib]) => !blackList.some((black) => lib.startsWith(black)))
+    )
+
+    fs.writeFileSync(`${sourceFolder}/package.json`, JSON.stringify({ ...pkg, devDependencies: removeBlackList }), {
+      encoding: 'utf-8',
+    })
+
+    // eslint-disable-next-line no-console
+    console.log(`Success update package devDependencies`)
+  } catch (e) {
+    throw new Error(`Error update package devDependencies: ${e}`)
   }
 }
 
@@ -88,5 +133,6 @@ async function gzip() {
 
 Promise.all(files)
   .then(() => console.log(`\nsuccess copy to ${sourceFolder}`))
+  .then(updatePackageDevDependencies)
   .then(createReadme)
   .then(gzip)
